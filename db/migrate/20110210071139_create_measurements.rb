671fe6a7de0976f3ca49e5548e4237166230be13
David Henner <davidhenner@drhenner.local>
2011-02-10 07:39:09 +0000

    add measurement migration

---
+++ db/migrate/20110210071139_create_measurements.rb
@@ -0,0 +1,18 @@
+class CreateMeasurements < ActiveRecord::Migration
+  def self.up
+    create_table :measurements do |t|
+      t.integer :user_id
+      t.decimal :shoe_size
+      t.decimal :waist
+      t.integer :pant_length
+      t.integer :dress_size
+      t.string :shirt_size
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :measurements
+  end
+end
