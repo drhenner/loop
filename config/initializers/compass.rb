dd74085ef6d148fcf268de85161842bee8018696
David Henner <drhenner@yahoo.com>
2011-04-05 23:15:39 -0700

    get compass working

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -2,15 +2,18 @@ require 'compass'
 require 'compass/app_integration/rails'
 #Compass::AppIntegration::Rails.initialize!
 
+if Rails.env == "production"
+  require 'fileutils'
+  FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 
-require 'fileutils'
-FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+  Compass::AppIntegration::Rails.initialize!
 
-Compass::AppIntegration::Rails.initialize!
+  Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+  Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
 
-Rails.configuration.middleware.delete('Sass::Plugin::Rack')
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
-
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
-    :urls => ['/stylesheets'],
-    :root => "#{Rails.root}/tmp")
+  Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+      :urls => ['/stylesheets'],
+      :root => "#{Rails.root}/tmp")
+else
+  Compass::AppIntegration::Rails.initialize!
+end
